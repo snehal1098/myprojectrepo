3440d226f37165dd2d31bf22096bad0ee9d10c2e
snehal1098 <snehal1098@gmail.com>
2021-03-23 10:55:47 +0530

    added br1func1

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -34,6 +34,12 @@ public class Calculator {
     public static void five(int num1){
         System.out.println(num1*num1*num1*num1*num1);
     }
+    public static void six(int num1){
+        System.out.println(num1*num1*num1*num1*num1*num1);
+    }
+    public static void br1func1(int num1){
+        System.out.println(num1*num1*num1*num1*num1*num1);
+    }
 
 
 }
