b04053fcd042538eb426a3bb8bdd4527b73d3ab8
snehal1098 <snehal1098@gmail.com>
2021-03-23 11:08:06 +0530

    added new br1func2

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -40,6 +40,10 @@ public class Calculator {
     public static void br1func1(int num1){
         System.out.println(num1*num1*num1*num1*num1*num1);
     }
-
+    
+    public static void br1func2(int num1){
+        System.out.println(num1*num1*num1*num1*num1*num1);
+    }
+    
 
 }
